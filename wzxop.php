dfba944fa50c0ec65ece4360a7de470608e9d913
wzxxcz <155713461+wzxxcz@users.noreply.github.com>
2025-03-29 12:12:11 +0800

    Add files via upload

---
+++ wzxop.php
@@ -0,0 +1,135 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>欧歌API</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta content="yes" name="apple-mobile-web-app-capable">
+    <meta content="black" name="apple-mobile-web-app-status-bar-style">
+    <meta name="format-detection" content="telephone=no">
+    <meta content="false" name="twcClient" id="twcClient">
+    <meta name="aplus-touch" content="1">
+   
+    <style>
+        body,html{width:100%;height:100%}
+        *{margin:0;padding:0}
+        body{background-color:#fff}
+        #browser img{
+            width:50px;
+        }
+        #browser{
+            margin: 0px 10px;
+            text-align:center;
+        }
+        #contens{
+            font-weight: bold;
+            color: #2466f4;
+            margin:-285px 0px 10px;
+            text-align:center;
+            font-size:20px;
+            margin-bottom: 50px;
+        }
+        .top-bar-guidance{font-size:15px;color:#fff;height:70%;line-height:1.8;padding-left:20px;padding-top:20px;background:url(/404/banner.png) center top/contain no-repeat}
+        .top-bar-guidance .icon-safari{width:25px;height:25px;vertical-align:middle;margin:0 .2em}
+        .app-download-tip{margin:0 auto;width:290px;text-align:center;font-size:15px;color:#2466f4;background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAcAQMAAACak0ePAAAABlBMVEUAAAAdYfh+GakkAAAAAXRSTlMAQObYZgAAAA5JREFUCNdjwA8acEkAAAy4AIE4hQq/AAAAAElFTkSuQmCC) left center/auto 15px repeat-x}
+        .app-download-tip .guidance-desc{background-color:#fff;padding:0 5px}
+        .app-download-tip .icon-sgd{width:25px;height:25px;vertical-align:middle;margin:0 .2em}
+        .app-download-btn{display:block;width:214px;height:40px;line-height:40px;margin:18px auto 0 auto;text-align:center;font-size:18px;color:#2466f4;border-radius:20px;border:.5px #2466f4 solid;text-decoration:none}
+    </style>
+</head>
+<body>
+
+<div class="top-bar-guidance">
+   
+</div>
+<div id="contens">
+<p>1.jar线路专用域名接口</p>
+<p><br/></p>
+<p>2.长期维护永远免费切勿贩卖</p>
+
+</div>
+<div class="app-download-tip">
+    <span class="guidance-desc">装逼专属域名接口，赶快来体验吧</span>
+    
+</div>
+<div class="app-download-tip">
+   <span class="guidance-desc">https://jar.u.xn--dkw.xn--6qq986b3xl/m/jar.php?id=og&ou=%E5%85%AC%E4%BC%97%E5%8F%B7%E6%AC%A7%E6%AD%8Capp;md5;f6411fd495912c546a91ea10c03a76e99?&mYt</span>
+    
+</div>
+
+
+
+
+<p><br/></p>
+<p><br/></p>
+<button class="app-download-btn" onclick="copy1()"  style="text-align: center; ">点此复制jar接口</a></button>
+
+<p><br/></p>
+<div class="app-download-tip">
+    <span class="guidance-desc">要分清多仓单仓单线食用</span>
+    
+</div>
+
+
+<p><br/></p>
+
+<button class="app-download-btn" onclick="copy2()"  style="text-align: center;">jar装逼网站</a></button>
+
+<script>
+function copy1() {
+  // 自定义的内容
+  var customContent1 = "https://jar.u.xn--dkw.xn--6qq986b3xl/m/jar.php?id=og&ou=%E5%85%AC%E4%BC%97%E5%8F%B7%E6%AC%A7%E6%AD%8Capp;md5;f6411fd495912c546a91ea10c03a76e99?&mYt";
+  
+  // 创建一个临时输入框元素
+  var tempInput = document.createElement("input");
+  tempInput.value = customContent1;
+  
+  // 将临时输入框元素添加到页面上
+  document.body.appendChild(tempInput);
+  
+  // 选择临时输入框中的文本
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // 兼容移动端
+  
+  // 复制选中的文本
+  document.execCommand("copy");
+  
+  // 删除临时输入框元素
+  document.body.removeChild(tempInput);
+  
+  // 提示复制成功
+  alert("已成功复制jar接口：" + customContent1);
+}
+
+function copy2() {
+  // 自定义的内容
+  var customContent2 = "https://jar.u.xn--dkw.xn--6qq986b3xl?&mYt";
+  
+  // 创建一个临时输入框元素
+  var tempInput = document.createElement("input");
+  tempInput.value = customContent2;
+  
+  // 将临时输入框元素添加到页面上
+  document.body.appendChild(tempInput);
+  
+  // 选择临时输入框中的文本
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // 兼容移动端
+  
+  // 复制选中的文本
+  document.execCommand("copy");
+  
+  // 删除临时输入框元素
+  document.body.removeChild(tempInput);
+  
+  // 提示复制成功
+  alert("已成功复制jar网址：" + customContent2);
+}
+
+
+</script>
+
+
+  
+</div>
+<body>
+</html>
